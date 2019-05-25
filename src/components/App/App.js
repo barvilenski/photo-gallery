de5e4226a68f07dfc15d304ebab9c41ba89f2a20
barvilenski <vbarv24@gmail.com>
2019-05-25 14:41:56 +0300

    Created initial version of the Gallery component and installed Axios

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SearchBar from '../SearchBar';
+import Gallery from '../Gallery';
 import './App.scss';
 
 class App extends React.Component {
@@ -12,6 +13,7 @@ class App extends React.Component {
     return (
       <div className="app-root">
         <SearchBar term={this.state.searchTerm} onSubmit={term => this.setState({ searchTerm: term })} />
+        <Gallery searchTerm={this.state.searchTerm} />
       </div>
     );
   }
---
+++ src/components/Gallery/Gallery.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import axios from 'axios';
+import './Gallery.scss';
+
+class Gallery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getImages = this.getImages.bind(this);
+    this.state = { images: [] };
+  }
+
+  componentDidUpdate() {
+    this.getImages();
+  }
+
+  async getImages() {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: this.props.searchTerm },
+      headers: { Authorization: 'Client-ID 09dd98b98246f12396ee47bd815221038330fb0fbcf70f499aecc775d74d29f4' }
+    });
+    this.setState({ images: response.data.results });
+  }
+
+  render() {
+    return (
+      <div className="gallery-root">
+        Images count: {this.state.images.length}
+    </div>
+    );
+  }
+}
+
+export default Gallery;
