0c156e49ddab7a2c4119a878263555a87ae1650c
barvilenski <vbarv24@gmail.com>
2019-05-25 13:56:15 +0300

    Created initial version of the SearchBar component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import SearchBar from '../SearchBar';
 import './App.scss';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { searchTerm: '' };
+  }
+
   render() {
-    return <div>App</div>;
+    return (
+      <div className="app-root">
+        <SearchBar term={this.state.searchTerm} onSubmit={term => this.setState({ searchTerm: term })} />
+      </div>
+    );
   }
 }
 
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './SearchBar.scss';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.state = { term: this.props.term };
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+  }
+
+  render() {
+    return (
+      <div className="search-bar-root">
+        <span>Unsplash Gallery</span>
+        <form onSubmit={this.onFormSubmit}>
+          <label>search icon placeholder</label>
+          <input
+            type="text"
+            placeholder="Search images"
+            value={this.state.term}
+            onChange={event => this.setState({ term: event.target.value })}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
